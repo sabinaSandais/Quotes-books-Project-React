60eef928f0cc520e2b3170d1dfd3ed894292a4e5
Sabina84 <129327140+Sabina84@users.noreply.github.com>
2024-04-09 14:25:14 +0200

    Removed extrsa

--- my-library/src/index.js
+++ my-library/src/index.js
@@ -5,22 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { AppContextProvider } from "./context/appContext";
-// import { SearchProvider } from "./context/SearchContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AppContextProvider>
-        {/* <SearchProvider> */}
         <App />
-        {/* </SearchProvider> */}
       </AppContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
